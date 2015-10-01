b54b9f21bcf9452ea3a2d5416eb66ce411c33e66
Joe Reich <reich.sj1@gmail.com>
2015-10-01 14:33:50 -0400

    Delete Users, links, etc.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,17 +11,23 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
       redirect_to @user
     else
+      flash[:danger] = @user.errors.full_messages
       render 'edit'
     end
   end
 
-  def delete
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
   end
 
   def index
-
+    @users = User.all
   end
 
   def show
@@ -31,9 +37,10 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      # flash[:success] = "Welcome to the Sample App!"
+      flash[:success] = "Welcome!"
       redirect_to @user
     else
+      flash[:danger] = @user.errors.full_messages
       render 'new'
     end
   end
