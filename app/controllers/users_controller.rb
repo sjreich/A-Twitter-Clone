b710e4b0b3bc7e70f24d38c80fcbe3059d6ba481
Joe Reich <reich.sj1@gmail.com>
2015-10-01 01:41:12 -0400

    Added user controller, signup, edit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,48 @@
+class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  def delete
+  end
+
+  def index
+
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      # flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
+
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,15 @@
 class User < ActiveRecord::Base
   has_secure_password
 
-  validates name:         presence: true,
+  validates :name,        presence: true,
                           length: {maximum: 55}
 
-  validates email:        presence: true,
-                          length: {maximum: 255}
-                          uniqueness: {case_sensitive: false}
+  email_regex = /[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}/i
+  validates :email,       presence: true,
+                          length: {maximum: 255},
+                          uniqueness: {case_sensitive: false},
+                          format: {with: email_regex}
 
-  validates password:     presence: true,
-                          length: {minumum: 6}
+  validates :password,    presence: true,
+                          length: {minimum: 6}
 end
